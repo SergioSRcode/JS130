6c46257d15ba5baf873ea35ec72557cd9f68455c
Sergio Ravera <sergioravera@hotmail.de>
2024-10-28 14:09:02 +0100

    rewrote the custom map function with reduce

---
+++ mapWithReduce.js
@@ -0,0 +1,22 @@
+function map(arr, callback, thisArg) {
+  // let newArr = [];
+
+  // for (let idx = 0; idx < arr.length; idx++) {
+  //   newArr.push(callback.call(thisArg, arr[idx]));
+  // }
+
+  return arr.reduce((acc, val) => {
+    acc.push(callback.call(thisArg, val));
+    return acc;
+  }, []);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
+console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
+console.log(map(numbers, () => false));
+// => [ false, false, false, false, false ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(map(values, value => String(value)));
+// => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
