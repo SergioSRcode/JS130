cb45151a2383362884f7e8ee2c6b29a95f63d7f5
Sergio Ravera <sergioravera@hotmail.de>
2024-10-28 10:38:41 +0100

    created custom map function

---
+++ customMap.js
@@ -0,0 +1,19 @@
+function map(arr, callback, thisArg) {
+  let newArr = [];
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    newArr.push(callback.call(thisArg, arr[idx]));
+  }
+
+  return newArr;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
+console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
+console.log(map(numbers, () => false));
+// => [ false, false, false, false, false ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(map(values, value => String(value)));
+// => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
