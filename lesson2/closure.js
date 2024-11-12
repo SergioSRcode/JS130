73eaea0632fdef15e09e4d7aba33afa0fedb1c57
Sergio Ravera <sergioravera@hotmail.de>
2024-11-12 15:25:26 +0100

    added an arrow function example for closures

--- lesson2/closure.js
+++ lesson2/closure.js
@@ -12,3 +12,18 @@ function createCounter() {
   const c2 = increment()
   const c3 = increment()
   console.log('example increment', c1, c2, c3)
+
+
+  // example 2 with error functions adding numbers
+
+let c = 4;
+// arrow func that immediately returns an arrow func.
+const addX = x => n => n + x; 
+// invoking first arrow func, receiving the value for "x" and returning
+// the func. "n => n + x"
+const addThree = addX(3);
+// invoking the second arrow func, receiving the value for "n"
+// adding n + x and returning the sum to "d";
+let d = addThree(c);
+// logging d === 4 + 3
+console.log('example partial application', d);
