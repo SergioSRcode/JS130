4fbb4455066f4f95db5b83c836b802ef79d57154
Sergio Ravera <sergioravera@hotmail.de>
2024-11-19 14:03:17 +0100

    added PP9

---
+++ lesson2/PP_Shorthands/PP9.js
@@ -0,0 +1,8 @@
+// Use array and object destructuring to extract the elements 
+// and properties from arr and obj into individual variables.
+
+const arr = [1, 2, 3];
+const obj = { a: 'a', b: 'b', c: 'c' };
+
+let [ num1, num2, num3 ] = arr;
+let { a, b, c } = obj;
