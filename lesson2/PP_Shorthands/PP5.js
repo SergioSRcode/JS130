ee4b45f8cdbafd46fc95a55aa9c887e8c81dd29d
Sergio Ravera <sergioravera@hotmail.de>
2024-11-19 13:41:31 +0100

    added PP5

---
+++ lesson2/PP_Shorthands/PP5.js
@@ -0,0 +1,13 @@
+// function product(num1, num2, num3) {
+//   return num1 * num2 * num3;
+// }
+
+// let array = [2, 3, 5];
+// let result = product(...array);
+
+function product(arr) {
+  return arr[0] * arr[1] * arr[2];
+}
+
+let array = [2, 3, 5];
+let result = product(array);
