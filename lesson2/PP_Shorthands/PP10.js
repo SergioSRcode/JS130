dd3dd27b4b1bfbfd1056af6fbb7037a273959407
Sergio Ravera <sergioravera@hotmail.de>
2024-11-19 14:07:51 +0100

    added PP11

---
+++ lesson2/PP_Shorthands/PP10.js
@@ -0,0 +1,10 @@
+// Use destructuring to extract the first element from arr 
+// to a new variable called first and extract the remaining 
+// elements into a new array called arr2.
+
+const arr = [1, 2, 3];
+
+const [ first, ...arr2 ] = arr;
+
+console.log(first);  // 1
+console.log(arr2); // [2, 3]
