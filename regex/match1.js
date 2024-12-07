e73266c9d94db39f6a9c947680949d613555c2ce
Sergio Ravera <sergioravera@hotmail.de>
2024-12-07 14:12:06 +0100

    added some tests

--- regex/match1.js
+++ regex/match1.js
@@ -1,4 +1,6 @@
 let mystr = "cast";
 
-if (mystr.match(/s/)) console.log("matched 's'");
-if (mystr.match(/x/)) console.log("matched 'x'");
+if (mystr.match(/s/)) console.log("matched 's'");  // conditional is truthy
+if (mystr.match(/s/)) console.log(mystr.match(/s/));  // loggs arr
+if (mystr.match(/x/)) console.log("matched 'x'");  // conditional is falsy
+console.log(mystr.match(/x/));  // loggs null
