dcb83bdbf05b366a40e9f18a123f9ed439623e73
Sergio Ravera <sergioravera@hotmail.de>
2024-12-10 18:27:27 +0100

    added exercise4

---
+++ regex/exercise4.js
@@ -0,0 +1,11 @@
+// Write a method that changes every arithmetic operator (+, -, *, /) 
+// to a '?' and returns the resulting string. Don't modify the original string.
+
+function mysteriousMath(str) {
+  return str.replace(/[+\-\*\/]/g, "?");
+}
+
+console.log(mysteriousMath('4 + 3 - 5 = 2'));
+// '4 ? 3 ? 5 = 2'
+console.log(mysteriousMath('(4 * 3 + 2) / 7 - 1 = 1'));
+// '(4 ? 3 ? 2) ? 7 ? 1 = 1'
